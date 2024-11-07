abf22a0820bcc83f93768fa83a1576243354b996
ldcumg <wonbin145@gmail.com>
2024-11-07 22:39:53 +0900

    update: isDone 추가

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,33 @@ function App() {
 	// input이랑 state 만들어서 value onChange로 엮기
 	const [text, setText] = useState("");
 
-	const textChange = (e) => {
-		setText(e.target.value);
-	};
-
-	// 버튼 만들어서 todos state에 input value 추가하기
-  const handleAddTodo = () => {
-    // todos 짝꿍인 setTodos로 todos 변경하기
-    // 스프레드 어쩌구로 기존에 있던 todos 풀어주고 input으로 받은 text로 새로운 todo 만들기
-		setTodos([...todos, { id: crypto.randomUUID(), title: text }]);
-  };
-  
 	return (
 		<>
-			<input type="text" value={text} onChange={textChange} />
-			<button onClick={handleAddTodo}>추가</button>
+			<input
+				type="text"
+				value={text}
+				onChange={(e) => {
+					setText(e.target.value);
+				}}
+			/>
+			<button
+				// 버튼 만들어서 todos state에 input value 추가하기
+				onClick={() => {
+					// todos 짝꿍인 setTodos로 todos 변경하기
+					// 스프레드 어쩌구로 기존에 있던 todos 풀어주고 input으로 받은 text로 새로운 todo 만들기
+					setTodos([...todos, { id: crypto.randomUUID(), title: text }]);
+				}}
+			>
+				추가
+			</button>
 			{todos.map((todo) => {
-				return <li key={todo.id}>{todo.title}</li>;
+				return (
+					<li key={todo.id}>
+						<h4 style={{ textDecoration: todo.isDone && "line-through" }}>
+							{todo.title}
+						</h4>
+					</li>
+				);
 			})}
 		</>
 	);
