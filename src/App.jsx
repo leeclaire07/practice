d1abb6e952f9ff32c29c554f4889221c0a705869
ldcumg <wonbin145@gmail.com>
2024-11-07 22:53:04 +0900

    update: isDone 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,38 @@ function App() {
 	const [todos, setTodos] = useState([]);
 	const [text, setText] = useState("");
 
-	const textChange = (e) => {
-		setText(e.target.value);
-	};
-	const handleAddTodo = () => {
-		setTodos([...todos, { id: crypto.randomUUID(), title: text }]);
-	};
-
-	// 삭제 함수 만들고 버튼에 엮기
-	const handleRemoveTodo = (target) => {
-		// create 때와 마찬가지로 todos를 변경하는 것이므로 setTodos를 사용한다
-		// filter를 이용해 todo.id !== target 조건식이 true인 객체만으로 이루어진 배열을 setTodos 해준다
-		setTodos(todos.filter((todo) => todo.id !== target));
-	};
-
 	return (
 		<>
-			<input type="text" value={text} onChange={textChange} />
-			<button onClick={handleAddTodo}>추가</button>
+			<input
+				type="text"
+				value={text}
+				onChange={(e) => {
+					setText(e.target.value);
+				}}
+			/>
+			<button
+				onClick={() => {
+					setTodos([...todos, { id: crypto.randomUUID(), title: text }]);
+				}}
+			>
+				추가
+			</button>
 			{todos.map((todo) => {
 				return (
 					<li key={todo.id}>
-						<h4>{todo.title}</h4>
-						<button onClick={() => handleRemoveTodo(todo.id)}>삭제</button>
+						<h4 style={{ textDecoration: todo.isDone && "line-through" }}>
+							{todo.title}
+						</h4>
+						<button
+							// 삭제 함수 만들고 버튼에 엮기
+							onClick={() => {
+								// create 때와 마찬가지로 todos를 변경하는 것이므로 setTodos를 사용한다
+								// filter를 이용해 todo.id !== target 조건식이 true인 객체만으로 이루어진 배열을 setTodos 해준다
+								setTodos(todos.filter((prev) => prev.id !== todo.id));
+							}}
+						>
+							삭제
+						</button>
 					</li>
 				);
 			})}
